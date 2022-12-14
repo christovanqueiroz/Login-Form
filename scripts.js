26610ae77f8f071340a2a8534917dc3bb3f2d629
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2022-12-14 17:21:46 -0300

    modified:   index.html 	new file:   scripts.js 	modified:   styles.css

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const emailInput = document.getElementById("emailInput");
+const passwordInput = document.getElementById("passwordInput");
+const passwordEye = document.getElementById("passwordEye");
+let eyeClosed = true;
+
+emailInput.addEventListener('focus', function(){
+    document.querySelector("#email img").src = "./assets/mail-focus.svg"
+})
+
+emailInput.addEventListener('blur', function(){
+    document.querySelector("#email img").src = "./assets/mail.svg"
+})
+
+passwordInput.addEventListener('focus', function(){
+    document.querySelector('#password img').src = "./assets/lock-focus.svg"
+})
+
+passwordInput.addEventListener('blur', function(){
+    document.querySelector('#password img').src = "./assets/lock.svg"
+})
+
+const handleToggleEye = () => {
+    if(eyeClosed) {
+        document.querySelector('#passwordEye svg path:nth-child(1)').style.stroke = "#FFC632";
+        document.querySelector('#passwordEye svg path:nth-child(2)').style.stroke = "#FFC632";
+        eyeClosed = true;
+    } else {
+        document.querySelector('#passwordEye svg path:nth-child(1)').style.stroke = "#AFB6C2";
+        document.querySelector('#passwordEye svg path:nth-child(2)').style.stroke = "#AFB6C2";
+        eyeClosed = false;
+    }
+}
+
+passwordEye.addEventListener('click', handleToggleEye);
